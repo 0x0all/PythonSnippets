73826dc6e529d7ecee6696e3f154342bcc8ab25a
Colin Lea <colincsl@gmail.com>
2013-02-01 10:59:36 -0500

    visvis example is now comparable to opencv's imshow() and waitkey()

--- visvisExample.py
+++ visvisExample.py
@@ -1,15 +1,16 @@
 import numpy as np
 import visvis as vv
-# import scipy.misc as sm
+from IPython import embed
 
-# im = imread('/Users/colin/Desktop/hog.png')
+# import scipy.misc as sm
+im = vv.imread('/Users/colin/Desktop/hog.png')
 
 # win = vv.imshow(im)
 # vv.processEvents()
 
 import time
 
-class WindowManager:
+class VideoViewer:
 
 	open_windows = []
 
@@ -26,22 +27,23 @@ class WindowManager:
 
 	def getWindowFromFigure(self, figure):
 		figs = [x['figure'] for x in self.open_windows]
-		# try:
-		index = figs.index(figure)
-		return self.open_windows[index]
-		# except:
-			# return None			
+		try:
+			index = figs.index(figure)
+			return self.open_windows[index]
+		except:
+			return None			
 
 	def keyHandler(self, event):
-		# from IPython import embed
 		# embed()
 		win = self.getWindowFromFigure(event.owner)
 		win['keyEvent'] = event.key
 		print event.text, event.key
 		
-	def createWindow(self, name, im):
-		vv.clf()
+	def createWindow(self, name, im, axis):
 
+		vv.figure()
+		vv.gca()
+		vv.clf()
 		fig = vv.imshow(im)
 		dims = im.shape
 
@@ -51,6 +53,22 @@ class WindowManager:
 		else:
 			fig.clim.Set(im.min(), im.max())
 		
+		fig.GetFigure().title = name
+		
+		''' Show ticks on axes? '''
+
+		if not axis:
+			fig.GetAxes().axis.visible = False
+			bgcolor = (0.,0.,0.)
+		else:
+			fig.GetAxes().axis.showBox = False
+			bgcolor = (1.,1.,1.)
+
+		fig.GetFigure().bgcolor = bgcolor
+		fig.GetAxes().bgcolor = bgcolor
+
+
+
 		fig.eventKeyUp.Bind(self.keyHandler)
 
 		win = {'name':name, 'figure':fig, 'shape':dims, 'keyEvent':None}
@@ -69,27 +87,29 @@ class WindowManager:
 
 		vv.update()
 
-	def imshow(self, name, im):
+	def imshow(self, name, im, axis=False):
 		'''
 		Inputs
 			name: string with figure name
 			im: numpy array
 		'''
 
-		if name not in self.open_windows:
-			win = self.createWindow(name, im)
-		else:
-			win = self.getWindow(name)
+		win = self.getWindow(name)
+		
+		if win is None:
+			win = self.createWindow(name, im, axis)
+		else:	
+			''' If new image is of different dimensions we must create new image'''
 			if im.shape != win['shape']:
 				self.destroyWindow(win['name'])
-				self.createWindow(name, im)
+				win = self.createWindow(name, im)
 
 		win['figure'].SetData(im)
+		vv.processEvents()
 
 	
 	def update(self):
 		''' Does not capture key inputs '''
-		vv.clf()
 		vv.processEvents()
 
 	def waitAndUpdate(self, duration=0):
@@ -111,7 +131,7 @@ class WindowManager:
 			duration = np.inf
 
 		start_time = time.time()
-		while(time.time()-start_time < duration):
+		while(time.time()-start_time < duration/1000.):
 			vv.processEvents()
 			time.sleep(.0001)
 
@@ -122,17 +142,17 @@ class WindowManager:
 		for i in self.open_windows:
 			i['keyEvent'] = None
 
-		print time.time()-start_time, duration
-
 		return 0
 
 
 
-w = WindowManager()
-w.imshow("Hi", np.eye(100, dtype=np.uint8))
+w = VideoViewer()
+w.imshow("Hi", np.eye(100, dtype=np.uint8), axis=True)
+w.imshow("im", im, axis=False)
 
 for j in range(100):
-	key = w.waitAndUpdate(-1)
+	key = w.waitAndUpdate(1000)
+	w.imshow("im", im+j*5, axis=False)
 	print key
 	
 
